f5e41abc94c0995c68afa5e5e3533f528178c692
Onwuzor Victor <victor.onwuzor@andela.com>
2020-03-13 15:31:09 +0100

    add database module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './core/database/database.module';
 
 @Module({
-  imports: [],
+  imports: [DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
