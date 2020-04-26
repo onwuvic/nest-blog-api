859713e33631bc78317cdd99d1774f3e073218bb
Onwuzor Victor <victor.onwuzor@andela.com>
2020-04-26 10:31:02 +0100

    add login and signup methods

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Body, Post, UseGuards, Request } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+
+import { AuthService } from './auth.service';
+import { UserDto } from '../users/dto/user.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private authService: AuthService) { }
+
+    @UseGuards(AuthGuard('local'))
+    @Post('login')
+    async login(@Request() req) {
+        return await this.authService.login(req.user);
+    }
+
+    @Post('signup')
+    async signUp(@Body() user: UserDto) {
+        return await this.authService.create(user);
+    }
+}
