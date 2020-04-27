f4f45984768c73e5dc77cabc8ba579a814f5e401
Onwuzor Victor <victor.onwuzor@andela.com>
2020-04-27 11:51:56 +0100

    add validation and unique email guard

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidateInputPipe } from './core/pipes/validate.pipe';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
+  // handle all user input validation globally
+  app.useGlobalPipes(new ValidateInputPipe());
   await app.listen(3000);
 }
 bootstrap();
--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -3,6 +3,7 @@ import { AuthGuard } from '@nestjs/passport';
 
 import { AuthService } from './auth.service';
 import { UserDto } from '../users/dto/user.dto';
+import { DoesUserExist } from '../../core/guards/doesUserExist.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -14,6 +15,7 @@ export class AuthController {
         return await this.authService.login(req.user);
     }
 
+    @UseGuards(DoesUserExist)
     @Post('signup')
     async signUp(@Body() user: UserDto) {
         return await this.authService.create(user);
